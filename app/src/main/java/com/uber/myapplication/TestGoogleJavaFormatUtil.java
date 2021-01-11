503ccbc1d015e0ce255a562abed8fc19dbc777a6
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-01-10 16:41:00 -0800

    Test TestGoogleJavaFormatUtil.java

---
+++ app/src/main/java/com/uber/myapplication/TestGoogleJavaFormatUtil.java
@@ -0,0 +1,27 @@
+package com.uber.myapplication;
+
+import androidx.annotation.NonNull;
+
+public class TestGoogleJavaFormatUtil {
+
+  @NonNull private Object onAttachInitialisedField;
+  @NonNull private Object mOnCreateViewInitialisedField;
+  @NonNull private Object mOnCreateInitialisedField;
+
+  private int test(
+      int a,
+      int b,
+      int c,
+      int d,
+      String message1,
+      String message2,
+      String message3,
+      String message4) {
+
+    // This is just comment with several blanket line above before the commit.
+    throw new RuntimeException();
+
+    // This is just another comment with several blanket line above before the commit.
+
+  }
+}
