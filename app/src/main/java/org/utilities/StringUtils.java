5a5f31f963e9ca6cbc09d5e498308c124e9476f1
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-29 23:04:55 -0800

    test

--- app/src/main/java/org/utilities/StringUtils.java
+++ app/src/main/java/org/utilities/StringUtils.java
@@ -4,7 +4,9 @@ import androidx.annotation.Nullable;
 
 public class StringUtils {
 
+  // test
   public static boolean isEmptyOrNull(@Nullable final CharSequence value) {
+
     return value == null || value.length() == 0;
   }
 }
