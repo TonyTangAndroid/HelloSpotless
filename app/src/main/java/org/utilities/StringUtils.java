bf04984b1d048fc03f6de7525cdf5faba1ee7f82
TonyTang <tonytang@uber.com>
2022-11-05 11:07:03 -0700

    Add MainClass

--- app/src/main/java/org/utilities/StringUtils.java
+++ app/src/main/java/org/utilities/StringUtils.java
@@ -7,6 +7,7 @@ public class StringUtils {
   /** test commit */
   public static boolean isEmptyOrNull(@Nullable final CharSequence value) {
 
+    // xx
     return value == null || value.length() == 0;
   }
 }
---
+++ javaDemo/src/test/java/com/uber/myapplication/MainClassTest.java
@@ -0,0 +1,12 @@
+package com.uber.myapplication;
+
+import org.junit.Test;
+
+public class MainClassTest {
+
+  @Test
+  public void onCreate() {
+
+    // test commit
+  }
+}
