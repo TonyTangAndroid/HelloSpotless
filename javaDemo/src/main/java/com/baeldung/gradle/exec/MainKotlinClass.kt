744d944f940055e9fe9a84ba2f7cfc7d9507448c
Zhilu Tang <tonythompsoncmu@gmail.com>
2024-12-24 09:08:39 -0800

    Confirm kotlin format is not supported.

--- javaDemo/src/main/java/com/baeldung/gradle/exec/MainKotlinClass.kt
+++ javaDemo/src/main/java/com/baeldung/gradle/exec/MainKotlinClass.kt
@@ -5,8 +5,13 @@ class MainKotlinClass {
   companion object {
     @JvmStatic
     fun main(args: Array<String>) {
-      println("Gradle command line arguments kotlin example")
+      println("Gradle command line arguments " +
+          "kotlin example")
       for (arg in args) {
+
+
+
+
         println("Got argument [$arg]")
       }
     }
